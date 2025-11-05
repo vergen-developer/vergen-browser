dbeec70d27901906062cdb498a58958a18ed976e
vergen-developer <gennaro.veropalumbo@vergen.it>
2025-11-05 14:57:22 +0100

    Updated ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -11,6 +11,8 @@ module.exports = {
       PORT: 3000,
       VITE_SUPABASE_URL: 'https://qxxfwoxtxscryithamlo.supabase.co',
       VITE_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4eGZ3b3h0eHNjcnlpdGhhbWxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNDE0NDgsImV4cCI6MjA3NzkxNzQ0OH0.c0HA-Y0DFrtqbLrFvuT3nZUqZ8_VvmbTjc_wKSzJWsU',
+      SUPABASE_URL: 'https://qxxfwoxtxscryithamlo.supabase.co',
+      SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4eGZ3b3h0eHNjcnlpdGhhbWxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNDE0NDgsImV4cCI6MjA3NzkxNzQ0OH0.c0HA-Y0DFrtqbLrFvuT3nZUqZ8_VvmbTjc_wKSzJWsU',
       JWT_SECRET: 'da93175701d2f4fea65d69b955907edd9a25c02ad8cfacb2adcd6f8e3b945c96'
     }
   }]
